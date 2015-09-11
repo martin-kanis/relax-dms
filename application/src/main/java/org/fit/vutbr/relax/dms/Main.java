35d5dcf27c51ecaf79a0a6894492c12e91904ac7
Martin Kanis <mkanis@redhat.com>
2015-09-11 12:00:35 +0200

    renamed ear directory

---
+++ application/src/main/java/org/fit/vutbr/relax/dms/Main.java
@@ -0,0 +1,12 @@
+package org.fit.vutbr.relax.dms;
+
+/**
+ * Enterprise Application Client main class.
+ *
+ */
+public class Main {
+    
+    public static void main( String[] args ) {
+        System.out.println( "Hello World Enterprise Application Client!" );
+    }
+}
