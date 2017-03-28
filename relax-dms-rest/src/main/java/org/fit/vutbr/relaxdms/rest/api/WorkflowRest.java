2f0c5054b2319f0b47a8173e8157e67c105d0b60
Martin Kanis <mkanis@redhat.com>
2017-03-28 22:55:32 +0200

    Workflow rest api

--- relax-dms-rest/src/main/java/org/fit/vutbr/relaxdms/rest/api/WorkflowRest.java
+++ relax-dms-rest/src/main/java/org/fit/vutbr/relaxdms/rest/api/WorkflowRest.java
@@ -1,9 +1,10 @@
 package org.fit.vutbr.relaxdms.rest.api;
 
-import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -11,61 +12,23 @@ import javax.ws.rs.core.Response;
  *
  * @author Martin Kanis
  */
-@Path("workflow/document")
+@Path("workflow")
 public interface WorkflowRest {
     
     @POST
-    @Path("/approve")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response approveDoc(String docData);
+    @Path("/fireUnfreeze")
+    public Response fireUnfreezeWorkflow();
     
     @POST
-    @Path("/decline")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response declineDoc(String docData);
+    @Path("/fireRelease")
+    public Response fireReleaseWorkflow();
+    
+    @GET
+    @Path("/getCustomRules")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getCustomRules();
     
     @POST
-    @Path("/submit/{manager}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response submitDoc(String docData, @PathParam("manager") String manager);
-    
-    @POST
-    @Path("/assign/{assignee}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response assignDoc(String docData, @PathParam("assignee") String assignee);
-    
-    @POST
-    @Path("/sign")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response signDoc(String docData);
-    
-    @POST
-    @Path("/freeze")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response freezeDoc(String docData);
-    
-    @POST
-    @Path("/release")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response releaseDoc(String docData);
-    
-    @POST
-    @Path("/startProgress")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response startProgress(String docData);
-    
-    @POST
-    @Path("/close")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response closeDoc(String docData);
-    
-    @POST
-    @Path("/reopen")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response openDoc(String docData);
-    
-    @POST
-    @Path("/addPermission/{user}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response addPermissionsToDoc(String docData, @PathParam("user") String user);
+    @Path("/fireCustom/{rule}")
+    public Response fireCustomWorkflow(@PathParam("rule") String rule);
 }
--- relax-dms-rest/src/main/java/org/fit/vutbr/relaxdms/rest/impl/WorkflowRestImpl.java
+++ relax-dms-rest/src/main/java/org/fit/vutbr/relaxdms/rest/impl/WorkflowRestImpl.java
@@ -1,8 +1,5 @@
 package org.fit.vutbr.relaxdms.rest.impl;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.ws.rs.core.Context;
@@ -11,13 +8,7 @@ import javax.ws.rs.core.SecurityContext;
 import org.fit.vutbr.relaxdms.api.security.AuthController;
 import org.fit.vutbr.relaxdms.api.service.DocumentService;
 import org.fit.vutbr.relaxdms.api.service.WorkflowService;
-import org.fit.vutbr.relaxdms.data.client.keycloak.api.KeycloakAdminClient;
-import org.fit.vutbr.relaxdms.data.db.dao.model.Document;
-import org.fit.vutbr.relaxdms.data.db.dao.model.DocumentMetadata;
 import org.fit.vutbr.relaxdms.data.db.dao.model.workflow.Environment;
-import org.fit.vutbr.relaxdms.data.db.dao.model.workflow.LabelEnum;
-import org.fit.vutbr.relaxdms.data.db.dao.model.workflow.StateEnum;
-import org.fit.vutbr.relaxdms.data.db.dao.model.workflow.Workflow;
 import org.fit.vutbr.relaxdms.rest.api.WorkflowRest;
 
 /**
@@ -36,276 +27,58 @@ public class WorkflowRestImpl implements WorkflowRest {
     @Inject
     private WorkflowService workflowService;
     
-    @Inject
-    private KeycloakAdminClient authClient;
-    
     @Context
     private SecurityContext sc;
     
     @Override
-    public Response approveDoc(String docData) {
-        try {
-            JsonNode jsonNode = new ObjectMapper().readValue(docData, JsonNode.class);
-            Document doc = jsonNodeToDocument(jsonNode);
-            String user = authController.getUserName(sc);
-            
-            if (authController.isUserAuthorized(sc, "manager")) {
-                if (workflowService.isSubmitted(doc.getWorkflow()) && !workflowService.isApproved(doc.getWorkflow())) {
-                    workflowService.approveDoc(doc, user);
-                    return Response.ok().build();
-                } else {
-                    return Response.status(403).entity("Forbidden: Can't approve document due wrong document's state!").build();
-                }
-            } else {
-                return Response.status(403).entity("Forbidden: Can't approve document due missing permissions!").build();
-            }    
-        } catch (IOException ex) {
-            return Response.status(400).entity("Jackson error: Could not serialize provided object!").build();
-        }
-    } 
-    
-    @Override
-    public Response declineDoc(String docData) {
-        try {
-            JsonNode jsonNode = new ObjectMapper().readValue(docData, JsonNode.class);
-            Document doc = jsonNodeToDocument(jsonNode);
-            String user = authController.getUserName(sc);
-            
-            if (authController.isUserAuthorized(sc, "manager")) {
-                if (workflowService.isSubmitted(doc.getWorkflow()) && !workflowService.isDeclined(doc.getWorkflow())) {
-                    workflowService.declineDoc(doc, user);
-                    return Response.ok().build();
-                } else {
-                    return Response.status(403).entity("Forbidden: Can't decline document due wrong document's state!").build();
-                }
-            } else {
-                return Response.status(403).entity("Forbidden: Can't decline document due missing permissions!").build();
-            }    
-        } catch (IOException ex) {
-            return Response.status(400).entity("Jackson error: Could not serialize provided object!").build();
+    public Response fireUnfreezeWorkflow() {
+        if (authController.isUserAuthorized(sc, "manager")) {
+            Environment env = new Environment();
+            env.setValue(true);
+            workflowService.insertAllFacts(documentService.getAllDocumentsMetadata(), env);
+            return Response.ok().build();  
+        } else {
+            return Response.status(403).entity("Forbidden: Can't fire unfreeze workflow due missing permissions!").build();
         }
     }
 
     @Override
-    public Response assignDoc(String docData, String assignee) {
-        try {
-            JsonNode jsonNode = new ObjectMapper().readValue(docData, JsonNode.class);
-            Document doc = jsonNodeToDocument(jsonNode);
-            String docId = jsonNode.get("_id").asText();
-            
-            if (authController.isUserAuthorized(sc, "writer") &&
-                    workflowService.isUserAuthorized(docId, authController.getUserName(sc))) {
-                if (authClient.userExists(assignee)) {
-                    workflowService.assignDocument(doc, assignee);
-                    return Response.ok().build();
-                } else {
-                    return Response.status(403).entity("Forbidden: Provided user does not exist!").build();
-                }
-            } else {
-                return Response.status(403).entity("Forbidden: Can't assign document due missing permissions!").build();
-            }    
-        } catch (IOException ex) {
-            return Response.status(400).entity("Jackson error: Could not serialize provided object!").build();
+    public Response fireReleaseWorkflow() {
+        if (authController.isUserAuthorized(sc, "manager")) {
+            Environment env = new Environment();
+            env.setValue(true);
+            env.setFireBy(authController.getUserName(sc));
+            workflowService.insertAllFacts(documentService.getAllDocumentsMetadata(), env);
+            return Response.ok().build();  
+        } else {
+            return Response.status(403).entity("Forbidden: Can't fire release workflow due missing permissions!").build();
         }
     }
 
     @Override
-    public Response submitDoc(String docData, String manager) {
-        try {
-            JsonNode jsonNode = new ObjectMapper().readValue(docData, JsonNode.class);
-            Document doc = jsonNodeToDocument(jsonNode);
-            String docId = jsonNode.get("_id").asText();
-            String user = authController.getUserName(sc);
-            
-            if (authController.isUserAuthorized(sc, "writer") &&
-                    workflowService.isUserAuthorized(docId, user)) {
-                if (authClient.getManagers().contains(manager)) {
-                    if (workflowService.checkState(doc.getWorkflow(), StateEnum.OPEN) ||
-                        workflowService.checkState(doc.getWorkflow(), StateEnum.IN_PROGRESS)) {
-                        Environment env = new Environment();
-                        env.setValue(true);
-                        env.setAssignTo(manager);
-                        env.setFireBy(user);
-                        workflowService.submitDocument(doc, env);
-                        return Response.ok().build();
-                    } else {
-                        return Response.status(403).entity("Forbidden: Can't sign document due wrong document's state!").build();
-                    }
-                } else {
-                    return Response.status(403).entity("Forbidden: Provided user does not exist or is not manager!").build();
-                }
-            } else {
-                return Response.status(403).entity("Forbidden: Can't submit document due missing permissions!").build();
-            }    
-        } catch (IOException ex) {
-            return Response.status(400).entity("Jackson error: Could not serialize provided object!").build();
-        }
-    }  
-
-    @Override
-    public Response signDoc(String docData) {
-        try {
-            JsonNode jsonNode = new ObjectMapper().readValue(docData, JsonNode.class);
-            Document doc = jsonNodeToDocument(jsonNode);
-            String user = authController.getUserName(sc);
-            
-            if (authController.isUserAuthorized(sc, "manager")) {
-                if (workflowService.canBeSigned(doc, true)) {
-                    workflowService.addLabel(doc, LabelEnum.SIGNED, user);
-                    return Response.ok().build();
-                } else {
-                    return Response.status(403).entity("Forbidden: Can't sign document due wrong document's state!").build();
-                }
-            } else {
-                return Response.status(403).entity("Forbidden: Can't sign document due missing permissions!").build();
-            }    
-        } catch (IOException ex) {
-            return Response.status(400).entity("Jackson error: Could not serialize provided object!").build();
+    public Response getCustomRules() {
+        if (authController.isUserAuthorized(sc, "manager")) {
+            return Response.ok(workflowService.getCustomRules()).build();  
+        } else {
+            return Response.status(403).entity("Forbidden: Can't get custom rules due missing permissions!").build();
         }
     }
 
     @Override
-    public Response freezeDoc(String docData) {
-        try {
-            JsonNode jsonNode = new ObjectMapper().readValue(docData, JsonNode.class);
-            Document doc = jsonNodeToDocument(jsonNode);
-            String user = authController.getUserName(sc);
-            StateEnum state = doc.getWorkflow().getState().getCurrentState();
-            
-            if (authController.isUserAuthorized(sc, "writer")) {
-                if (state == StateEnum.OPEN || state == StateEnum.IN_PROGRESS && 
-                        !workflowService.checkLabel(doc.getWorkflow(), LabelEnum.FREEZED)) {
-                    workflowService.addLabel(doc, LabelEnum.FREEZED, user);
-                    return Response.ok().build();
-                } else {
-                    return Response.status(403).entity("Forbidden: Can't freeze document due wrong document's state!").build();
-                }
+    public Response fireCustomWorkflow(String rule) {
+        if (authController.isUserAuthorized(sc, "manager")) {
+            if (workflowService.getCustomRules().contains(rule)) {
+                Environment env = new Environment();
+                env.setValue(true);
+                env.setFireBy(authController.getUserName(sc));
+                env.setRule(rule);
+                workflowService.insertAllFacts(documentService.getAllDocumentsMetadata(), env);
+                return Response.ok().build();
             } else {
-                return Response.status(403).entity("Forbidden: Can't freeze document due missing permissions!").build();
-            }    
-        } catch (IOException ex) {
-            return Response.status(400).entity("Jackson error: Could not serialize provided object!").build();
+                return Response.status(400).entity("Provided rule does not exist!").build();
+            }
+        } else {
+            return Response.status(403).entity("Forbidden: Can't get custom rules due missing permissions!").build();
         }
-    }
-
-    @Override
-    public Response releaseDoc(String docData) {
-        try {
-            JsonNode jsonNode = new ObjectMapper().readValue(docData, JsonNode.class);
-            Document doc = jsonNodeToDocument(jsonNode);
-            String user = authController.getUserName(sc);
-            
-            if (authController.isUserAuthorized(sc, "manager")) {
-                if (workflowService.checkLabel(doc.getWorkflow(), LabelEnum.SIGNED) &&
-                    !workflowService.checkLabel(doc.getWorkflow(), LabelEnum.RELEASED)) {
-                    workflowService.addLabel(doc, LabelEnum.RELEASED, user);
-                    return Response.ok().build();
-                } else {
-                    return Response.status(403).entity("Forbidden: Can't release document due wrong document's state!").build();
-                }
-            } else {
-                return Response.status(403).entity("Forbidden: Can't release document due missing permissions!").build();
-            }    
-        } catch (IOException ex) {
-            return Response.status(400).entity("Jackson error: Could not serialize provided object!").build();
-        }
-    }
-
-    @Override
-    public Response startProgress(String docData) {
-        try {
-            JsonNode jsonNode = new ObjectMapper().readValue(docData, JsonNode.class);
-            Document doc = jsonNodeToDocument(jsonNode);
-            String user = authController.getUserName(sc);
-            
-            if (authController.isUserAuthorized(sc, "writer")) {
-                if (workflowService.checkState(doc.getWorkflow(), StateEnum.OPEN)) {
-                    workflowService.changeState(doc, StateEnum.IN_PROGRESS, user);
-                    return Response.ok().build();
-                } else {
-                    return Response.status(403).entity("Forbidden: Can't start progress on document due wrong document's state!").build();
-                }
-            } else {
-                return Response.status(403).entity("Forbidden: Can't start progress on document due missing permissions!").build();
-            }    
-        } catch (IOException ex) {
-            return Response.status(400).entity("Jackson error: Could not serialize provided object!").build();
-        }
-    }
-
-    @Override
-    public Response closeDoc(String docData) {
-        try {
-            JsonNode jsonNode = new ObjectMapper().readValue(docData, JsonNode.class);
-            Document doc = jsonNodeToDocument(jsonNode);
-            String user = authController.getUserName(sc);
-            
-            if (authController.isUserAuthorized(sc, "writer")) {
-                if (!workflowService.checkState(doc.getWorkflow(), StateEnum.CLOSED) &&
-                    !workflowService.checkLabel(doc.getWorkflow(), LabelEnum.RELEASED)) {
-                    workflowService.changeState(doc, StateEnum.CLOSED, user);
-                    return Response.ok().build();
-                } else {
-                    return Response.status(403).entity("Forbidden: Can't close document due wrong document's state!").build();
-                }
-            } else {
-                return Response.status(403).entity("Forbidden: Can't close document due missing permissions!").build();
-            }    
-        } catch (IOException ex) {
-            return Response.status(400).entity("Jackson error: Could not serialize provided object!").build();
-        }
-    }
-
-    @Override
-    public Response openDoc(String docData) {
-        try {
-            JsonNode jsonNode = new ObjectMapper().readValue(docData, JsonNode.class);
-            Document doc = jsonNodeToDocument(jsonNode);
-            String user = authController.getUserName(sc);
-            
-            if (authController.isUserAuthorized(sc, "writer")) {
-                if (workflowService.checkState(doc.getWorkflow(), StateEnum.CLOSED)) {
-                    workflowService.changeState(doc, StateEnum.OPEN, user);
-                    return Response.ok().build();
-                } else {
-                    return Response.status(403).entity("Forbidden: Can't reopen document due wrong document's state!").build();
-                }
-            } else {
-                return Response.status(403).entity("Forbidden: Can't reopen document due missing permissions!").build();
-            }    
-        } catch (IOException ex) {
-            return Response.status(400).entity("Jackson error: Could not serialize provided object!").build();
-        }
-    }
-
-    @Override
-    public Response addPermissionsToDoc(String docData, String user) {
-        try {
-            JsonNode jsonNode = new ObjectMapper().readValue(docData, JsonNode.class);
-            Document doc = jsonNodeToDocument(jsonNode);
-            String docId = jsonNode.get("_id").asText();
-            
-            if (authController.isUserAuthorized(sc, "writer") &&
-                    workflowService.isUserAuthorized(docId, authController.getUserName(sc))) {
-                if (authClient.userExists(user)) {
-                    workflowService.addPermissionsToDoc(doc, user);
-                    return Response.ok().build();
-                } else {
-                    return Response.status(403).entity("Forbidden: Provided user does not exist!").build();
-                }
-            } else {
-                return Response.status(403).entity("Forbidden: Can't add permissions to document due missing permissions!").build();
-            }    
-        } catch (IOException ex) {
-            return Response.status(400).entity("Jackson error: Could not serialize provided object!").build();
-        }
-    }
-    
-    private Document jsonNodeToDocument(JsonNode jsonNode) {
-        byte[] data = documentService.getDataFromJson(jsonNode);
-        byte[] attachments = documentService.getAttachmentsFromJson(jsonNode);
-        DocumentMetadata metadata = documentService.getMetadataFromJson(jsonNode);
-        Workflow workflow = workflowService.getWorkflowFromJson(jsonNode);
-        return new Document(data, attachments, metadata, workflow);
     }
 }
